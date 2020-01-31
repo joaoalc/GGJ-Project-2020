55595cc3ad2f49b00dbb57f7c85ffacb9c96b270
Samuel <diogosamuelfernandes@outlook.com>
2020-01-31 22:03:15 +0000

    Some changes

---
+++ Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMove : MonoBehaviour {
+    public float moveSpeed;
+    public float jumpHeight;
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        else rb.velocity = new Vector2(0, rb.velocity.y);
+    }
+}
